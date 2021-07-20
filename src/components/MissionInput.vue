bd4d70e84764d34515b25d0cf3cba57605b21f0a
laxative.hsieh <laxative.hsieh@yi-spaces.com>
2021-07-20 10:22:18 +0800

    Feat: apart MissionInput section from BaseFeatureLayout

---
+++ src/components/MissionInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="mission-input-wrapper">
+    <input
+      type="text"
+      class="mission-input-content"
+      placeholder="add a new mission"
+    />
+    <div class="mission-input-add-icon">+</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+$mission_input_font_color: #00a7ff;
+$mission_input_div_height: 45px;
+
+.mission-input {
+  &-wrapper {
+    background-color: #fff;
+    width: 100%;
+    height: $mission_input_div_height;
+
+    display: flex;
+    color: $mission_input_font_color;
+  }
+
+  &-content {
+    border: 0;
+    width: 85%;
+    height: 100%;
+    padding: 5px 10px;
+    line-height: $mission_input_div_height;
+    color: $mission_input_font_color;
+    font-weight: bold;
+
+    &:focus-visible {
+      outline: none;
+    }
+
+    &::placeholder {
+      color: $mission_input_font_color;
+      font-weight: bold;
+      font-style: italic;
+      text-transform: uppercase;
+    }
+  }
+
+  &-add-icon {
+    width: 15%;
+    min-width: 30px;
+    height: 100%;
+    text-align: center;
+    line-height: $mission_input_div_height;
+    font-weight: bold;
+    font-size: 16px;
+    cursor: pointer;
+  }
+}
+</style>
---
+++ src/interfaces/Mission.ts
@@ -0,0 +1,5 @@
+export default interface Mission {
+  complete: boolean;
+  name: string;
+  remainTime: number; // using seconds
+}
