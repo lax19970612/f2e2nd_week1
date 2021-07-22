5b36261ed21b0c4312e77a4748d54cd8f1800626
laxative.hsieh <laxative.hsieh@yi-spaces.com>
2021-07-22 17:20:42 +0800

    Feat: apart PlayingPanel from AdvancedFeatureLayout

---
+++ src/components/AdvancedFeature/PlayingPanel.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="playing-panel">
+    <div class="timer-controller-wrapper relative">
+      <div class="timer-controller-outer">
+        <div class="timer-controller-inner">
+          <div class="button-start" />
+        </div>
+      </div>
+    </div>
+    <div class="timer-content-wrapper relative">
+      <div class="timer-content-remain-time">25:00</div>
+      <div class="timer-content-mission">THE FIRST THING TO DO TODAY</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$button_size: 36px;
+
+.playing-panel {
+  position: absolute;
+  bottom: 0;
+  left: 40px;
+  transform: translate(0, 50%);
+
+  width: 350px;
+  height: 350px;
+}
+
+.timer-controller {
+  &-inner,
+  &-outer {
+    background-color: #ff4384;
+    border: 7px solid #003164;
+    border-radius: 50%;
+    padding: 2px;
+  }
+
+  &-outer {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 120px;
+    height: 120px;
+    cursor: pointer;
+
+    transition: 0.3s ease;
+    &:hover {
+      background-color: #fff;
+    }
+  }
+
+  &-inner {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .button-pause {
+      width: 0;
+      height: $button_size;
+      border-style: double;
+      border-width: 0 0 0 36px;
+      border-color: #fff;
+      margin-left: 0;
+
+      transition: all 0.3s ease;
+    }
+
+    .button-start {
+      width: 0;
+      height: $button_size;
+      margin-left: $button_size/4;
+      border-style: solid;
+      border-width: $button_size/2 0 $button_size/2 $button_size;
+      border-color: transparent transparent transparent #fff;
+      transition: all 0.3s ease;
+    }
+  }
+}
+
+.timer-content {
+  &-wrapper {
+    height: 350px;
+    background-color: #ffedf7;
+    border-radius: 50%;
+    z-index: -1;
+
+    display: flex;
+    flex-direction: column;
+    padding-top: 50px;
+  }
+
+  &-remain-time {
+    color: #ff4384;
+    font-size: 64px;
+    font-weight: bold;
+    text-align: center;
+  }
+  &-mission {
+    color: #003164;
+    font-size: 16px;
+    font-weight: bold;
+    text-align: center;
+    text-transform: uppercase;
+    margin-top: -15px;
+  }
+}
+</style>
--- src/layouts/AdvancedFeatureLayout.vue
+++ src/layouts/AdvancedFeatureLayout.vue
@@ -5,19 +5,7 @@
         :advancedMode="advancedMode"
         @menuItemClickEmit="menuItemClickEmitHandler"
       />
-      <div class="playing-panel" v-if="advancedMode">
-        <div class="timer-controller-wrapper relative">
-          <div class="timer-controller-outer">
-            <div class="timer-controller-inner">
-              <div class="button-start" />
-            </div>
-          </div>
-        </div>
-        <div class="timer-content-wrapper relative">
-          <div class="timer-content-remain-time">25:00</div>
-          <div class="timer-content-mission">THE FIRST THING TO DO TODAY</div>
-        </div>
-      </div>
+      <playing-panel v-if="advancedMode" />
     </div>
     <div class="table__cell__6 menu-wrapper">
       <div class="todolist-wrapper" v-if="currentPage === 'todolist'">
@@ -67,6 +55,7 @@ import MissionInput from "@/components/MissionInput.vue";
 import MissionList from "@/components/MissionList.vue";
 
 import MenuGroup from "@/components/AdvancedFeature/MenuGroup.vue";
+import PlayingPanel from "@/components/AdvancedFeature/PlayingPanel.vue";
 
 import Mission from "@/interfaces/Mission";
 
@@ -76,6 +65,7 @@ export default defineComponent({
     MissionInput,
     MissionList,
     MenuGroup,
+    PlayingPanel,
   },
   props: {
     advancedMode: Boolean,
@@ -136,8 +126,6 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-$button_size: 36px;
-
 .advanced-feature-layout {
   position: absolute;
   top: 0;
@@ -149,98 +137,6 @@ $button_size: 36px;
   height: 100vh;
   background-color: #003164;
   transition: left 0.5s ease;
-}
-
-.playing-panel {
-  position: absolute;
-  bottom: 0;
-  left: 40px;
-  transform: translate(0, 50%);
-
-  width: 350px;
-  height: 350px;
-}
-
-.timer-controller {
-  &-inner,
-  &-outer {
-    background-color: #ff4384;
-    border: 7px solid #003164;
-    border-radius: 50%;
-    padding: 2px;
-  }
-
-  &-outer {
-    position: absolute;
-    top: 0;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 120px;
-    height: 120px;
-    cursor: pointer;
-
-    transition: 0.3s ease;
-    &:hover {
-      background-color: #fff;
-    }
-  }
-
-  &-inner {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .button-pause {
-      width: 0;
-      height: $button_size;
-      border-style: double;
-      border-width: 0 0 0 36px;
-      border-color: #fff;
-      margin-left: 0;
-
-      transition: all 0.3s ease;
-    }
-
-    .button-start {
-      width: 0;
-      height: $button_size;
-      margin-left: $button_size/4;
-      border-style: solid;
-      border-width: $button_size/2 0 $button_size/2 $button_size;
-      border-color: transparent transparent transparent #fff;
-      transition: all 0.3s ease;
-    }
-  }
-}
-
-.timer-content {
-  &-wrapper {
-    height: 350px;
-    background-color: #ffedf7;
-    border-radius: 50%;
-    z-index: -1;
-
-    display: flex;
-    flex-direction: column;
-    padding-top: 50px;
-  }
-
-  &-remain-time {
-    color: #ff4384;
-    font-size: 64px;
-    font-weight: bold;
-    text-align: center;
-  }
-  &-mission {
-    color: #003164;
-    font-size: 16px;
-    font-weight: bold;
-    text-align: center;
-    text-transform: uppercase;
-    margin-top: -15px;
-  }
 }
 
 .logo {
